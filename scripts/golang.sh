97bdf13855e58add560037c3402f945f130e9fea
Leo Neumeyer <leoneu@apache.org>
2014-08-25 12:35:56 -0700

    Add more Go packages to script.

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -2,9 +2,15 @@
 
 # Installs or updates go development tools.
 go get -u -x code.google.com/p/go.tools/cmd/goimports
-go get -u -x code.google.com/p/go.tools/oracle
+go get -u -x code.google.com/p/go.tools/cmd/oracle
 go get -u -x code.google.com/p/rog-go/exp/cmd/godef
 go get -u -x github.com/nsf/gocode
-go get -u -x github.com/dominikh/go-errcheck.el
-go get -u -x github.com/golang/glog
+go get -u -x github.com/kisielk/errcheck
 go get -u -x github.com/dougm/goflymake
+
+# Libraries.
+go get -u -x github.com/golang/glog
+go get -u -x github.com/gonum/blas
+go get -u -x github.com/gonum/matrix
+go get -u -x github.com/gonum/floats
+go get -u -x github.com/gonum/graph
