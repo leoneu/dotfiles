c046f796f702218ffe966ba84e1a52d5f427502f
Leo Neumeyer <leoneu@apache.org>
2014-08-25 08:48:07 -0700

    Add script to install go tools.

---
+++ scripts/golang.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Installs or updates go development tools.
+go get -u -x code.google.com/p/go.tools/cmd/goimports
+go get -u -x code.google.com/p/go.tools/oracle
+go get -u -x code.google.com/p/rog-go/exp/cmd/godef
+go get -u -x github.com/nsf/gocode
+go get -u -x github.com/dominikh/go-errcheck.el
+go get -u -x github.com/golang/glog
+go get -u -x github.com/dougm/goflymake
