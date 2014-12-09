bce3e01982a804882fbdba3b1cc72b8972d22f68
Leo Neumeyer <leoneu@apache.org>
2014-12-09 11:29:26 -0800

    Update emacs packages.

---
+++ emacs/.emacs.d/elpa/dash-20141207.1052/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (21639 19636 201810 946000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ emacs/.emacs.d/elpa/ess-20141208.1757/ess-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ess" "20141208.1757" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
