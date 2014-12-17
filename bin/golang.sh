012bd260ef0e16891b11d064fad0bdbc7ae808f9
Leo Neumeyer <leoneu@apache.org>
2014-12-17 13:02:25 -0800

    Modify golang install script.

--- bin/golang.sh
+++ bin/golang.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Installs or updates go development tools.
-go get -u -x code.google.com/p/go.tools/cmd/goimports
-go get -u -x code.google.com/p/go.tools/cmd/oracle
-go get -u -x code.google.com/p/rog-go/exp/cmd/godef
-go get -u -x github.com/nsf/gocode
-go get -u -x github.com/kisielk/errcheck
-go get -u -x github.com/dougm/goflymake
-go get -u -x github.com/golang/lint/golint
 
-# Libraries.
-go get -u -x github.com/golang/glog
-go get -u -x github.com/gonum/blas
-go get -u -x github.com/gonum/matrix
-go get -u -x github.com/gonum/floats
-go get -u -x github.com/gonum/graph
+GOBIN=go
+GET="$GOBIN get -u -x"
+INSTALL="$GOBIN install "
+
+PACKAGES="golang.org/x/tools/cmd/goimports golang.org/x/tools/cmd/gorename golang.org/x/tools/cmd/eg golang.org/x/tools/cmd/oracle golang.org/x/tools/cmd/cover golang.org/x/tools/cmd/stringer code.google.com/p/rog-go/exp/cmd/godef github.com/nsf/gocode github.com/kisielk/errcheck github.com/dougm/goflymake github.com/golang/lint/golint github.com/golang/glog github.com/gonum/blas github.com/gonum/matrix/mat64 github.com/gonum/floats github.com/gonum/graph"
+
+for p in $PACKAGES; do
+    echo ">>> " $p
+    $GET $p
+    $INSTALL $p
+done
