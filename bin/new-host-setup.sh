4c86fc8794ca82106b419d4e10825bd5af7dfb87
Leo Neumeyer <leoneu@amazon.com>
2015-09-14 12:34:27 -0700

    Modify new host script.

--- bin/new-host-setup.sh
+++ bin/new-host-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+~/dotfiles/bin/gitconfig.sh
 ln -sf $HOME/dotfiles/emacs/.emacs.d $HOME
-ln -sf $HOME/dotfiles/git/.gitconfig $HOME
 ln -sf $HOME/dotfiles/git/.gitignore_global $HOME
 ln -sf $HOME/dotfiles/tmux/.tmux.conf $HOME
 ln -sf $HOME/dotfiles/tmux/.tmux $HOME
